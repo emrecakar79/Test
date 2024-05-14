5239fd3c3b87b3cfc92e529d20987ddf5b69e1d7
Emre Cakar <emre.cakar@hs-hannover.de>
2024-05-14 11:14:45 +0200

    Zusammengesetzte Operatoren werden verwendet

--- Main.java
+++ Main.java
@@ -7,13 +7,13 @@ public class Main
         int summe = 0;
         for (int i= 1 ; i <= 10 ; ++i)
         {
-            summe = summe + i;
+            summe += i;
         }
         
         int mult = 1;
         for (int i = 2; i <= 10 ; ++i)
         {
-            mult = mult * i;
+            mult *= i;
         }
     }
 }
