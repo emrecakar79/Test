2b605af6fb0e1c0debd7e358428f588e71eb29b3
Emre Cakar <emre.cakar@hs-hannover.de>
2024-05-14 11:25:34 +0200

    Variable og = 10

--- Main.java
+++ Main.java
@@ -4,14 +4,15 @@ public class Main
 {
     public static void main(String args[])
     {
+        int og = 10;
         int summe = 0;
-        for (int i= 1 ; i <= 10 ; ++i)
+        for (int i= 1 ; i <= og ; ++i)
         {
             summe += i;
         }
         
         int mult = 1;
-        for (int i = 2; i <= 10 ; ++i)
+        for (int i = 2; i <= og ; ++i)
         {
             mult *= i;
         }
