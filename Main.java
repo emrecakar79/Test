fed4c3d81c30cf4b90761f321a3014f10334c840
Emre Cakar <emre.cakar@hs-hannover.de>
2025-11-24 14:59:40 +0100

    Ausgabe ist aktualisiert

--- Main.java
+++ Main.java
@@ -18,6 +18,6 @@ public class Main
             mult = mult * i;
         }
         
-        System.out.println("Summe = " + summe + " Mult = " + mult);
+        System.out.println("Summe: " + summe + " Mult:" + mult);
     }
 }
