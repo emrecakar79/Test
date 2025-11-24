f3eba543dae74687bfa98aa4d5b38b11cc7217a4
Emre Cakar <emre.cakar@hs-hannover.de>
2025-11-24 14:44:16 +0100

    Summe ist implementiert

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main
 {
     public static void main(String args[])
     {
+        int summe = 0;
+        int og = 10;
         
+        for (int i = 1; i <= og ; ++i)
+        {
+            summe = summe + i;
+        }
     }
 }
